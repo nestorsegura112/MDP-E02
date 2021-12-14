e3f064ddb08c67aec780e3d83cf192ff0fc67dc0
JhostynG <92282123+JhostynG@users.noreply.github.com>
2021-12-14 14:20:18 -0500

    Add files via upload

---
+++ personajes/FabricaAbstracta.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos1.personajes;
+
+/**
+ *
+ * @author jhosd
+ */
+public interface FabricaAbstracta {
+    
+    public Arma CrearArma();
+    
+    public Armadura CrearArmadura();
+
+    public Personaje CrearPersonaje();
+    
+    public Montura CrearMontura();
+}
---
+++ personajes/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos1.personajes;
+
+/**
+ *
+ * @author jhosd
+ */
+public class Main{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI obj=new GUI();
+        
+    }
+    
+}
